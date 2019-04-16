8f77ac313b82718f8e60ca495e64e63d8a9a4252
vadymdk <vadim.dk81@gmail.com>
2019-04-16 12:49:58 +0300

    initCommit

---
+++ piano.js
@@ -0,0 +1,167 @@
+///
+//const container = document.querySelector('.buttons-container');
+//
+//container.onkeydown = (e) => {
+//   const target = e.target; // элемент который вызвал событие
+//   const btn = target.getAttribute('data-btn');
+//   }
+function animateKey(){
+    const container = document.querySelector('.buttons-container');
+   
+    
+        this.animate([
+         {transform: 'translateY(0px)'},
+         {transform: 'translateY(10px)'}
+     ], { 
+          duration: 300,
+          iterations: 1
+        }); 
+    
+    
+    let self = this;
+    
+container.addEventListener('keydown', function(e) {
+   let target = e.target; // элемент который вызвал событие
+   let action = target.getAttribute('data-btn');
+    if (action) {
+        self[action]();
+     }
+    
+   });
+//   document.querySelector('.buttons-container')
+       
+
+}
+document.addEventListener('keydown', function(event) {
+    
+    if ( event.code == 'KeyA') {
+        
+     
+        animateKey();
+        audioA.play();
+        console.log('A');
+  }  else if (event.code == 'KeyS') {
+      document.getElementById('buttonS').animate([
+         {transform: 'translateY(0px)'},
+         {transform: 'translateY(10px)'}
+     ], { 
+          duration: 300,
+          iterations: 1
+        });
+        
+        audioS.play();
+    
+       console.log('S');
+  } else if (event.code == 'KeyD') {
+      document.getElementById('buttonD').animate([
+         {transform: 'translateY(0px)'},
+         {transform: 'translateY(10px)'}
+     ], { 
+          duration: 300,
+          iterations: 1
+        });
+        
+        audioD.play();
+      
+    console.log('D');
+  } else if (event.code == 'KeyF') {
+      document.getElementById('buttonF').animate([
+         {transform: 'translateY(0px)'},
+         {transform: 'translateY(10px)'}
+     ], { 
+          duration: 300,
+          iterations: 1
+        });
+        
+        audioF.play();
+    console.log('F');
+  } else if (event.code == 'KeyG') {
+      document.getElementById('buttonG').animate([
+         {transform: 'translateY(0px)'},
+         {transform: 'translateY(10px)'}
+     ], { 
+          duration: 300,
+          iterations: 1
+        });
+        
+        audioG.play();
+    console.log('G');
+  } else if (event.code == 'KeyH') {
+      document.getElementById('buttonH').animate([
+         {transform: 'translateY(0px)'},
+         {transform: 'translateY(10px)'}
+     ], { 
+          duration: 300,
+          iterations: 1
+        });
+        
+        audioH.play();
+    console.log('H');
+  } else if (event.code == 'KeyJ') {
+      document.getElementById('buttonJ').animate([
+         {transform: 'translateY(0px)'},
+         {transform: 'translateY(10px)'}
+     ], { 
+          duration: 300,
+          iterations: 1
+        });
+        
+        audioJ.play();
+    console.log('J');
+  } else if (event.code == 'KeyK') {
+      document.getElementById('buttonK').animate([
+         {transform: 'translateY(0px)'},
+         {transform: 'translateY(10px)'}
+     ], { 
+          duration: 300,
+          iterations: 1
+        });
+        
+        audioK.play();
+    console.log('K');
+  }
+     
+    });
+
+
+
+
+const audioA = document.getElementById("myAudio-A");
+function playAudio() { 
+  audioA.play(); 
+} 
+
+const audioS = document.getElementById("myAudio-S");
+function playAudio() { 
+  audioS.play(); 
+} 
+
+const audioD = document.getElementById("myAudio-D");
+function playAudio() { 
+  audioD.play(); 
+} 
+
+const audioF = document.getElementById("myAudio-F");
+function playAudio() { 
+  audioF.play(); 
+} 
+
+const audioG = document.getElementById("myAudio-G");
+function playAudio() { 
+  audioG.play(); 
+} 
+
+const audioH = document.getElementById("myAudio-H");
+function playAudio() { 
+  audioH.play(); 
+} 
+
+const audioJ = document.getElementById("myAudio-J");
+function playAudio() { 
+  audioJ.play(); 
+} 
+
+const audioK = document.getElementById("myAudio-K");
+function playAudio() { 
+  audioK.play(); 
+} 
